a7349a3b33718239355eb511716faee4046ae3c1
Zac Crumpton <z.crumpton@gmail.com>
2020-05-18 18:32:45 -0500

    componenets done

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.scss';
+import BoardContainer from '../components/BoardContainer/BoardContainer';
+import MyNavbar from '../components/MyNavbar/MyNavbar';
+import Auth from '../components/Auth/Auth';
 
 function App() {
   return (
     <div className="App">
       <h2>PIN THE TREST</h2>
-      <button className="btn btn-dark">TEST</button>
+      <MyNavbar/>
+      <Auth/>
+      <BoardContainer/>
     </div>
   );
 }
