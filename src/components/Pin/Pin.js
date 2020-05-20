1086c94f89a9edd05ed74fc9502b6bd2227fb004
Zac Crumpton <z.crumpton@gmail.com>
2020-05-19 20:44:08 -0500

    connected pins data from firebase

---
+++ src/components/Pin/Pin.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Pin.scss';
+
+class Pin extends React.Component {
+  render() {
+    return (
+      <div className="Pin">
+        ima pin not a pig
+      </div>
+    );
+  }
+}
+
+export default Pin;
--- src/components/SingleBoard/SingleBoard.js
+++ src/components/SingleBoard/SingleBoard.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './SingleBoard.scss';
+import boardsData from '../../helpers/data/boardsData';
+import pinsData from '../../helpers/data/pinsData';
+import Pin from '../Pin/Pin';
 
 class SingleBoard extends React.Component {
   static propTypes = {
@@ -8,14 +11,37 @@ class SingleBoard extends React.Component {
     setSingleBoard: PropTypes.func.isRequired,
   }
 
+  state = {
+    board: {},
+    pins: [],
+  }
+
+  componentDidMount() {
+    const { boardId } = this.props;
+    boardsData.getSingleBoard(boardId)
+      .then((request) => {
+        const board = request.data;
+        this.setState({ board });
+        pinsData.getPinsByBoardId(boardId)
+          .then((pins) => this.setState({ pins }));
+      })
+      .catch((err) => console.error('unable to get single board', err));
+  }
+
   render() {
-    const { boardId, setSingleBoard } = this.props;
+    const { setSingleBoard } = this.props;
+    const { board, pins } = this.state;
+
+    const makePins = pins.map((p) => <Pin key={p.id} pin={p}/>);
 
     return (
       <div className="SingleBoard">
         <button className="btn btn-danger" onClick={() => { setSingleBoard(''); }}>X</button>
-        <h2>SingleBoard view</h2>
-      <h3>{boardId}</h3>
+        <h2>{board.name} Board</h2>
+        <h2>{board.description}</h2>
+        <div className="d-flex flex-wrap">
+          {makePins}
+        </div>
       </div>
     );
   }
