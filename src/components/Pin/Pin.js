ae01e2ee1d38b741eb8f3c83e5b4f08a7e5505b3
Zac Crumpton <z.crumpton@gmail.com>
2020-05-19 20:55:31 -0500

    pins display

--- src/components/Pin/Pin.js
+++ src/components/Pin/Pin.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './Pin.scss';
+import pinShape from '../../helpers/propz/pinShape';
 
 class Pin extends React.Component {
+  static propTypes = {
+    pin: pinShape.pinShape,
+  }
+
   render() {
+    const { pin } = this.props;
     return (
-      <div className="Pin">
-        ima pin not a pig
+      <div className="Pin col-3">
+        <div className="card">
+          <img className="card-img-top" src={pin.imageUrl} alt="pin"/>
+          <div className="card-body">
+            <h5 className="card-title">{pin.title}</h5>
+          </div>
+        </div>
       </div>
     );
   }
